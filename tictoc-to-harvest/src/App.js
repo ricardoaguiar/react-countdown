46c1c46816ff34fbf4c1ef2c4b38b08a0cc84d0f
ricardo aguiar <2197501+ricardoaguiar@users.noreply.github.com>
2020-02-21 19:26:04 +0100

    init commit

---
+++ tictoc-to-harvest/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Tictoc } from './Tictoc';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <h5>TICTOC TO HARVEST</h5>
+        <Tictoc />
+      </header>
+    </div>
+  );
+}
+export default App;
